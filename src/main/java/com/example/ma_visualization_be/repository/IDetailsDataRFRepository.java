77596abdbb287d044f5347340d652cfb7c005828
Viet Ta <89030236+viet1012@users.noreply.github.com>
2025-07-14 10:24:13 +0700

    upload

--- src/main/java/com/example/ma_visualization_be/repository/IDetailsDataRFRepository.java
+++ src/main/java/com/example/ma_visualization_be/repository/IDetailsDataRFRepository.java
@@ -12,60 +12,60 @@ public interface IDetailsDataRFRepository extends JpaRepository<DummyEntity, Lon
     @Query(value = """
            DECLARE  @month VARCHAR(6)  = REPLACE(:month, '-', '');
            DECLARE @div NVARCHAR(10) = :dept
-           SELECT\s
-            COALESCE(ratio.DIV,
-            CASE
-                WHEN XBLNR2 LIKE '1566-%' THEN\s
-                    CASE
-                        WHEN LEFT(KOSTL,6) IN ('614100','614000') THEN 'PRESS'				
-                        WHEN LEFT(KOSTL,6) = '614200' THEN 'MOLD'				
-                        WHEN LEFT(KOSTL,6) IN ('614600', '614700') THEN 'GUIDE'	
-                    END
-                ELSE 'OTHER'
-            END) as Dept,
-
-            IIF(AUFNR LIKE 'M%',RIGHT(AUFNR,LEN(AUFNR)-1),AUFNR) As MacID,
-            mac.MACHINE_NAME as MacName,
-            SGTXT as Cate,
-            iss.MATNR,
-            part.MAKTX,
-            CONVERT(DATE,BLDAT,112) as UseDate,
-            KOSTL,
-            KONTO,
-            XBLNR2,
-            BKTXT,
-            ERFMG as QTY,
-            ERFME as UNIT,
-            IIF(ERFMG<0,-1,1)*AMOUNT*COALESCE(ratio.Ratio,1) as ACT,
-            'Shared ' + Format(ratio.Ratio,'0.0%') as Note
-           FROM MANUFASPCPD.dbo.MANUFA_F_PD_DTM_ISSUE iss
-           INNER JOIN MANUFASPCPD.dbo.MANUFA_F_PD_GRB_PART part ON iss.MATNR = part.MATNR
-           LEFT JOIN F2Database.dbo.F2_MA_MACHINE_MASTER mac ON IIF(AUFNR LIKE 'M%',RIGHT(AUFNR,LEN(AUFNR)-1),AUFNR) = mac.CODE
-           LEFT JOIN F2Database.dbo.F2_Cost_Center_Share share ON iss.KOSTL = share.Cost_Center
-           LEFT JOIN F2Database.dbo.F2_Cost_Center_Share_Ratio ratio ON share.Group_Share = ratio.Dept
-                                                                    AND CASE
-                                                                        WHEN XBLNR2 LIKE '1566-%' THEN\s
-                                                                            CASE
-                                                                                WHEN LEFT(KOSTL,6) IN ('614100','614000') THEN 'PRESS'				
-                                                                                WHEN LEFT(KOSTL,6) = '614200' THEN 'MOLD'				
-                                                                                WHEN LEFT(KOSTL,6) IN ('614600', '614700') THEN 'GUIDE'	
-                                                                            END
-                                                                        ELSE 'OTHER'
-                                                                        END <> 'OTHER'\s
-           WHERE KONTO = '570600'
-           AND LEFT(BLDAT,6) = @month
-           AND BLDAT < CONVERT(varchar,GETDATE(),112)
-           AND COALESCE(ratio.DIV,
-            CASE
-                WHEN XBLNR2 LIKE '1566-%' THEN\s
-                    CASE
-                        WHEN LEFT(KOSTL,6) IN ('614100','614000') THEN 'PRESS'				
-                        WHEN LEFT(KOSTL,6) = '614200' THEN 'MOLD'				
-                        WHEN LEFT(KOSTL,6) IN ('614600', '614700') THEN 'GUIDE'	
-                    END
-                ELSE 'OTHER'
-            END) = @div
-           ORDER BY ACT Desc
+            SELECT
+             COALESCE(ratio.DIV,
+             CASE
+              WHEN XBLNR2 LIKE '1566-%' THEN\s
+               CASE
+                WHEN LEFT(KOSTL,6) IN ('614100','614000') THEN 'PRESS'   \s
+                WHEN LEFT(KOSTL,6) = '614200' THEN 'MOLD'   \s
+                WHEN LEFT(KOSTL,6) IN ('614600', '614700') THEN 'GUIDE'\s
+               END
+              ELSE 'OTHER'
+             END) as Dept,
+             IIF(AUFNR LIKE 'M%',RIGHT(AUFNR,LEN(AUFNR)-1),AUFNR) As MacID,
+             mac.MACHINE_TYPE as MacName,
+             SGTXT as Cate,
+             iss.MATNR,
+             part.MAKTX,
+             CONVERT(DATE,BLDAT,112) as UseDate,
+             KOSTL,
+             KONTO,
+             XBLNR2,
+             BKTXT,
+             ERFMG as QTY,
+             ERFME as UNIT,
+             IIF(ERFMG<0,-1,1)*AMOUNT*COALESCE(ratio.Ratio,1) as ACT,
+             'Shared ' + Format(ratio.Ratio,'0.0%') as Note
+            FROM MANUFASPCPD.dbo.MANUFA_F_PD_DTM_ISSUE iss
+            INNER JOIN MANUFASPCPD.dbo.MANUFA_F_PD_GRB_PART part ON iss.MATNR = part.MATNR
+            LEFT JOIN F2Database.dbo.F2_MA_MACHINE_MASTER mac ON IIF(AUFNR LIKE 'M%',RIGHT(AUFNR,LEN(AUFNR)-1),AUFNR) = mac.CODE
+            LEFT JOIN F2Database.dbo.F2_Cost_Center_Share share ON iss.KOSTL = share.Cost_Center
+            LEFT JOIN F2Database.dbo.F2_Cost_Center_Share_Ratio ratio ON share.Group_Share = ratio.Dept
+                           AND CASE
+                            WHEN XBLNR2 LIKE '1566-%' THEN\s
+                             CASE
+                              WHEN LEFT(KOSTL,6) IN ('614100','614000') THEN 'PRESS'   \s
+                              WHEN LEFT(KOSTL,6) = '614200' THEN 'MOLD'   \s
+                              WHEN LEFT(KOSTL,6) IN ('614600', '614700') THEN 'GUIDE'\s
+                             END
+                            ELSE 'OTHER'
+                            END <> 'OTHER'\s
+            WHERE KONTO = '570600'
+            AND LEFT(BLDAT,6) = @month
+            AND BLDAT < CONVERT(varchar,GETDATE(),112)
+            AND COALESCE(ratio.DIV,
+             CASE
+              WHEN XBLNR2 LIKE '1566-%' THEN\s
+               CASE
+                WHEN LEFT(KOSTL,6) IN ('614100','614000') THEN 'PRESS'   \s
+                WHEN LEFT(KOSTL,6) = '614200' THEN 'MOLD'   \s
+                WHEN LEFT(KOSTL,6) IN ('614600', '614700') THEN 'GUIDE'\s
+               END
+              ELSE 'OTHER'
+             END) = @div
+            ORDER BY ACT Desc
+          
             """, nativeQuery = true)
     List<IDetailsDataRFDTO> getDailyDetailsRepairFee(@Param("month") String month, @Param("dept") String dept);
 
